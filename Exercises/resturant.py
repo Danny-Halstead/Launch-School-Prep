55e9064f0bb8daf37db9a8b2484ba86ebb9f7591
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-10 20:45:06 -0500

    Some lottery games

--- Exercises/resturant.py
+++
@@ -1,7 +0,0 @@
-class Resturant():
-    def __init__(self, name, food_served):
-        self.name = name
-        self.food_served = food_served
-
-    def describe_resturant(self):
-        print(f'{self.name} serves {self.food_served}.')
